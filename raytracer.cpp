9f6a6f6415a06f56d198fd81d42a168df613a1c4
karimx10 <karimsalama94@hotmail.com>
2017-02-15 16:08:22 +0100

    bismillah

---
+++ raytracer.cpp
@@ -0,0 +1,297 @@
+#include "raytracer.h"
+#include "scene_types.h"
+#include "ray.h"
+#include "image.h"
+#include "kdtree.h"
+#include <stdio.h>
+float pi=M_PI;
+float IOR=1;
+#define TMAX 100000
+
+/// acne_eps is a small constant used to prevent acne when computing intersection
+//  or boucing (add this amount to the position before casting a new ray !
+const float acne_eps = 1e-4;
+
+bool intersectPlane(Ray *ray, Intersection *intersection, Object *obj) {
+	//! \todo : compute intersection of the ray and the plane object
+	vec3 n=obj->geom.plane.normal;
+	vec3 d=ray->dir;
+	if(dot(n,d)==0)
+		return false;
+	float t= -(dot(n,ray->orig)+obj->geom.plane.dist)/dot(n,d);
+	if(t<=ray->tmin || t>ray->tmax)
+		return false;
+	ray->tmax=t;
+	intersection->normal=n;
+	intersection->mat=&(obj->mat);
+	intersection->position=rayAt(*ray,t);
+	return true;
+}
+
+bool intersectSphere(Ray *ray, Intersection *intersection, Object *obj) {
+
+	//! \todo : compute intersection of the ray and the sphere object
+	vec3 co= ray->orig-obj->geom.sphere.center;
+
+	float r=obj->geom.sphere.radius, b=2.0*dot(ray->dir,co), c=dot(co,co)-r*r,
+		delta=b*b-4.0*c;
+	float t1,t2;
+	if(delta<0) return false;
+	else{
+		t1=(-b+sqrt(delta))/2.f;
+		t2=(-b-sqrt(delta))/2.f;
+	}
+	float t;
+	if(t1*t2<0) t= t1<t2? t2:t1;
+	else t= t1<t2? t1:t2;
+	if(t<=ray->tmin || t>ray->tmax)
+		return false;
+	ray->tmax=t;
+	point3 P=rayAt(*ray,t);
+	intersection->normal=normalize(P-obj->geom.sphere.center);
+	if(t1*t2<0) intersection->normal= -intersection->normal;//flip the normal vector if the camera is inside the sphere
+	intersection->position=P+acne_eps*intersection->normal;
+	intersection->mat=&(obj->mat);
+	return true;
+}
+
+bool intersectCylinder(Ray *ray, Intersection *intersection, Object *obj){
+	vec3 D = obj->geom.cylinder.direction;
+	vec3 C = obj->geom.cylinder.center;
+	float r = obj->geom.cylinder.radius;
+	vec3 d= ray->dir-dot(ray->dir,D)*D;
+	vec3 dp= ray->orig-C;
+	vec3 cc=dp-dot(dp,D)*D;
+	float a= dot(d,d),
+		b=2.f*dot(d,dp-dot(dp,D)*D),
+		c= dot(cc,cc)-r*r;
+	float delta = b*b -4.f*a*c;
+	float t1,t2;
+	if(delta<0) return false;
+	else{
+		t1=(-b+sqrt(delta))/(2.0*a);
+		t2=(-b-sqrt(delta))/(2.0*a);
+	}
+	float t;
+	if(t1*t2<0) t= t1<t2? t2:t1;
+	else t= t1<t2? t1:t2;
+	if(t<=ray->tmin || t>ray->tmax)
+		return false;
+	point3 P=rayAt(*ray,t);
+	point3 pc=C+((dot(P-C,D))/dot(D,D))*D;
+	float l=obj->geom.cylinder.length/2;
+	if(length(pc-C)>l){
+		Ray rTest; Intersection iTest;
+		rayInit(&rTest,ray->orig,ray->dir);
+		bool b= intersectPlane(&rTest, &iTest, initPlane(D,-dot(D,C+l*D),obj->mat));
+		b= intersectPlane(&rTest, &iTest, initPlane(-D,-dot(-D,C-l*D),obj->mat))||b;
+		if(length((C+l*D)-iTest.position)<=r||length((C-l*D)-iTest.position)<=r){
+			ray->tmax=length(iTest.position-ray->orig)/length(ray->dir);
+			intersection->position=iTest.position;
+			intersection->normal=iTest.normal;
+			intersection->mat=&(obj->mat);
+			return true;
+		}
+		return false;
+	}
+	intersection->normal=normalize(P-pc);
+	if(t1*t2<0) intersection->normal= -intersection->normal;//flip the normal vector if the camera is inside the cylinder
+	intersection->position=P+acne_eps*intersection->normal;
+	ray->tmax=t;
+	intersection->mat=&(obj->mat);
+	return true;
+}
+
+bool intersectScene(const Scene *scene, Ray *ray, Intersection *intersection) {
+	bool hasIntersection = false;
+	int objectCount = scene->objects.size();
+	//!\todo loop on each object of the scene to compute intersection
+	for(int i=0;i<objectCount;i++){
+		switch(scene->objects[i]->geom.type){
+			case PLANE:
+				hasIntersection|=intersectPlane(ray,intersection,scene->objects[i]);
+			break;
+			case SPHERE:
+				hasIntersection|=intersectSphere(ray,intersection,scene->objects[i]);
+			break;
+			case CYLINDER:
+				hasIntersection|=intersectCylinder(ray,intersection,scene->objects[i]);
+			break;
+			default:break;
+		}
+	}
+	return hasIntersection;
+}
+
+/* --------------------------------------------------------------------------- */
+/*
+ *	The following functions are coded from Cook-Torrance bsdf model description and are suitable only
+ *  for rough dielectrics material (RDM. Code has been validated with Mitsuba renderer)
+ */
+
+float RDM_chiplus(float c){
+	return (c > 0.f) ? 1.f : 0.f;
+}
+
+/** Normal Distribution Function : Beckmann
+ * NdotH : Norm . Half
+ */
+float RDM_Beckmann(float NdotH, float alpha){
+	//! \todo compute Beckmann normal distribution
+	float cos2x=NdotH*NdotH;
+	float tan2x=(1-cos2x)/cos2x;
+	return exp(-tan2x/(alpha*alpha))/(pi*alpha*alpha*cos2x*cos2x);
+}
+
+// Fresnel term computation. Implantation of the exact computation. we can use the Schlick approximation
+// LdotH : Light . Half
+float RDM_Fresnel(float LdotH, float extIOR, float intIOR){
+	//! \todo compute Fresnel term
+	float f=1-((extIOR/intIOR)*(extIOR/intIOR)*(1-LdotH*LdotH));
+	if(f<0)
+		return 1.f;
+	float t=sqrt(f);
+	float f1=extIOR*LdotH-intIOR*t,
+	f2=extIOR*LdotH+intIOR*t,
+	f3=extIOR*t-intIOR*LdotH,
+	f4=extIOR*t+intIOR*LdotH,
+	Rs=(f1*f1)/(f2*f2),
+	Rp=(f3*f3)/(f4*f4);
+	return .5*(Rs+Rp);
+}
+
+float RDM_G1(float DdotH, float DdotN, float alpha) {
+	//!\todo compute G1 term of the Smith fonction
+	float b=1.f/(alpha*(sqrt(1-DdotN*DdotN)/DdotN));
+	float k=DdotH/DdotN;
+	if(k<=0)
+		return 0.f;
+	if(b<1.6)
+		return 1.f;
+	return (3.535*b+ 2.181*b*b)/(1+2.276*b+2.577*b*b);
+}
+
+float RDM_Smith(float LdotH, float LdotN, float VdotH, float VdotN, float alpha){
+	//!\todo the Smith fonction
+	return RDM_G1(LdotH,LdotN,alpha)*RDM_G1(VdotH,VdotN,alpha);
+}
+
+// Specular term of the Cook-torrance bsdf
+// LdotH : Light . Half
+// NdotH : Norm . Half
+// VdotH : View . Half
+// LdotN : Light . Norm
+// VdotN : View . Norm
+color3 RDM_bsdf_s(float LdotH, float NdotH, float VdotH, float LdotN, float VdotN, Material *m){
+	//!\todo specular term of the bsdf, using D = RDB_Beckmann, F = RDM_Fresnel, G = RDM_Smith
+	return m->specularColor*(
+		RDM_Beckmann(NdotH,m->roughness)*
+		RDM_Fresnel(LdotH,IOR,m->IOR)*
+		RDM_Smith(LdotH,LdotN,VdotH,VdotN, m->roughness)
+		)/(4.f*LdotN*VdotN);
+}
+// diffuse term of the cook torrance bsdf
+color3 RDM_bsdf_d(Material *m) {
+	//!\todo compute diffuse component of the bsdf
+	return m->diffuseColor/pi;
+}
+
+// The full evaluation of bsdf(wi, wo) * cos (thetai)
+// LdotH : Light . Half
+// NdotH : Norm . Half
+// VdotH : View . Half
+// LdotN : Light . Norm
+// VdtoN : View . Norm
+// compute bsdf * cos(Oi)
+color3 RDM_bsdf(float LdotH, float NdotH, float VdotH, float LdotN, float VdotN, Material *m){
+	//! \todo compute bsdf diffuse and specular term
+	return RDM_bsdf_d(m)+RDM_bsdf_s(LdotH,NdotH,VdotH,LdotN,VdotN,m);
+}
+
+/* --------------------------------------------------------------------------- */
+
+color3 shade(vec3 n, vec3 v, vec3 l, color3 lc, Material *mat){
+	//! \todo compute bsdf, return the shaded color taking into account the lightcolor
+	if(dot(l,n)<0)
+		return color3(0.f);
+	vec3 h= normalize((v+l)/length(v+l));
+	float LdotH=dot(l,h), NdotH=dot(n,h), VdotH=dot(v,h), LdotN=dot(l,n), VdotN=dot(v,n);
+	return lc*RDM_bsdf(LdotH,NdotH,VdotH,LdotN,VdotN,mat)*LdotN;
+}
+
+//! if tree is not null, use intersectKdTree to compute the intersection instead of intersect scene
+color3 trace_ray(Scene * scene, Ray *ray, KdTree *tree) {
+	color3 ret = scene->skyColor;
+	Intersection intersection;
+	if(intersectScene(scene, ray, &intersection)){
+		if(intersection.mat->diffuseColor==color3(0.03, 0.3, 0.03))
+			return intersection.normal;
+		ret=color3(0.f);
+		point3 P=intersection.position;
+		for(unsigned int i=0;i<scene->lights.size();i++){
+			point3 L= scene->lights[i]->position;
+			vec3 l =normalize(L-P);
+			Ray rOmbre; Intersection dummyInter;
+			rayInit(&rOmbre,P+acne_eps*l,l,0,length(P-L));
+			if(!intersectScene(scene,&rOmbre,&dummyInter))
+				ret+=shade(intersection.normal,-ray->dir, l, scene->lights[i]->color,intersection.mat);
+		}
+		if(ray->depth>0){
+			vec3 ref=normalize(reflect(ray->dir,intersection.normal));
+			rayInit(ray,P+acne_eps*ref,ref,ray->tmin,TMAX,ray->depth-1);
+			ret+=RDM_Fresnel(dot(ref,intersection.normal),IOR,intersection.mat->IOR)*trace_ray(scene,ray,tree);
+		}
+	}
+	return ret;
+}
+
+void renderImage(Image *img, Scene *scene) {
+	//! This function is already operational, you might modify it for antialiasing and kdtree initializaion
+	float aspect = 1.f/scene->cam.aspect;
+
+	KdTree *tree =  NULL;
+
+	//! \todo initialize KdTree
+
+	float delta_y = 1.f / (img->height * 0.5f); //! one pixel size
+	vec3 dy = delta_y * aspect * scene->cam.ydir; //! one pixel step
+	vec3 ray_delta_y = (0.5f - img->height * 0.5f) / (img->height * 0.5f) * aspect * scene->cam.ydir;
+
+	float delta_x = 1.f / (img->width * 0.5f);
+	vec3 dx = delta_x * scene->cam.xdir;
+	vec3 ray_delta_x = (0.5f - img->width * 0.5f) / (img->width * 0.5f) *scene->cam.xdir;
+
+	color3 *c= (color3*)malloc((img->width+1)*(img->height+1)*sizeof(color3));
+	for(size_t j=0; j<img->height+1; j++){
+		if(j!=0) printf("\033[A\r");
+		float progress = (float)j/img->height*100.f;
+		printf("progress\t[");
+		int cpt = 0;
+		for(cpt = 0; cpt<progress; cpt+=5) printf("-");
+		for(       ; cpt<100; cpt+=5) printf(" ");
+		printf("]\n");
+		#pragma omp parallel for
+		for(size_t i=0; i<img->width+1; i++) {
+			vec3 ray_dir = scene->cam.center + ray_delta_x + ray_delta_y + float(i)*dx + float(j)*dy;
+			Ray rx;
+			rayInit(&rx, scene->cam.position, normalize(ray_dir),0,TMAX,3);
+			c[i+j*(img->width+1)] = trace_ray(scene, &rx, tree);
+		}
+	}
+	printf("Antialiasing\n");
+	for(size_t j=0; j<img->height; j++){
+		if(j!=0) printf("\033[A\r");
+		float progress = (float)j/img->height*100.f;
+		printf("progress\t[");
+		int cpt = 0;
+		for(cpt = 0; cpt<progress; cpt+=5) printf("-");
+		for(       ; cpt<100; cpt+=5) printf(" ");
+		printf("]\n");
+		#pragma omp parallel for
+		for(size_t i=0; i<img->width; i++) {
+			color3 *ptr = getPixelPtr(img, i,j);
+			*ptr = (c[i+j*(img->width+1)]+c[(i+1)+j*(img->width+1)]+c[i+(j+1)*(img->width+1)]+c[(i+1)+(j+1)*(img->width+1)])/4.f;
+		}
+	}
+	free(c);
+}
